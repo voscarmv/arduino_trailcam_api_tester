b79360c548bb2ac2cdc1e3707b103d9d1ebb7b48
Oscar Mier <voscarmv@gmail.com>
2025-04-30 04:27:28 +0000

    Perfected token regeneration

--- notifications.js
+++ notifications.js
@@ -24,51 +24,45 @@ dotenv.config();
         }
         console.log(output);
 
-
         const token = `${output.data.token}`;
         const cableUrl = `${process.env.WS}/cable?token=${token}`;
-
         const ws = new WebSocket(cableUrl);
 
-        ws.on('open', function open() {
+        ws.on('open', async function open() {
             console.log('Connected to the server');
-            // Send a message to the server
-            // ws.send(JSON.stringify({ type: 'greeting', payload: 'Hello from Node.js client!' }));
             const msg = {
                 command: "subscribe",
                 identifier: JSON.stringify({
-                  channel: "NotificationsChannel",
-                //   room: "general"
+                    channel: "NotificationsChannel",
+                    //   room: "general"
                 })
-              };
-              ws.send(JSON.stringify(msg));
+            };
+            ws.send(JSON.stringify(msg));
+            // Try to reset token here
+
+            let output2;
+            let headers2;
+            try {
+                const response = await fetch(url, {
+                    method: 'PUT',
+                    headers: {
+                        'Content-Type': 'application/json',
+                        'Authorization': `Bearer ${token}`,
+                    }
+                });
+                output2 = await response.json();
+                headers2 = response.headers;
+            } catch (e) {
+                console.error(e)
+            }
+
+            console.log(output2);
+            console.log(headers2.get('authorization'));
         });
 
         ws.on('message', function incoming(data) {
-          console.log('Received:', data.toString());
+            console.log('Received:', data.toString());
         });
-
-        // const consumer = createConsumer(cableUrl);
-        // const subscription = consumer.subscriptions.create(
-        //     { channel: "Notifications" },
-        //     {
-        //         connected() {
-        //             console.log("Connected to ChatChannel.");
-        //         },
-        //         disconnected() {
-        //             console.log("Disconnected from ChatChannel.");
-        //         },
-        //         received(data) {
-        //             console.log("Received:", data);
-        //         },
-        //         // speak(message) {
-        //         //     this.perform("speak", { message: message });
-        //         // },
-        //     }
-        // );
-
-        //         // To send a message:
-        // subscription.speak("Hello, world!");
     }
 )();
 
